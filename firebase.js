6de68c27f0b054efb2854ce3041a1377f3865df5
Parth Hasmukh Jain <91812749+parthjain99@users.noreply.github.com>
2023-06-21 15:25:55 -0400

    rh

--- firebase.js
+++ firebase.js
@@ -1,13 +1,17 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvfO1VJTCzxslO5LHKKIeY5lSPPuzw9SM",
-  authDomain: "whatsapp-2-ce8ba.firebaseapp.com",
-  projectId: "whatsapp-2-ce8ba",
-  storageBucket: "whatsapp-2-ce8ba.appspot.com",
-  messagingSenderId: "103836882999",
-  appId: "1:103836882999:web:bf6ae47af74d50a688c7b2"
+  apiKey: "AIzaSyCVAHtSRAJos5ANphN0my2xtnaDf46q3oc",
+  authDomain: "whatsapp-711ce.firebaseapp.com",
+  projectId: "whatsapp-711ce",
+  storageBucket: "whatsapp-711ce.appspot.com",
+  messagingSenderId: "63514611719",
+  appId: "1:63514611719:web:f924cf7790c568d3c99aa2",
+  measurementId: "G-GCZCJ9Y7RS"
 };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
